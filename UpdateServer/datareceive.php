fe0056de99a655e2facc2104a8d5701d7f983e75
Tim Fischbach <59933715+timfischbach@users.noreply.github.com>
2022-05-10 12:45:33 +0200

    Add files via upload

--- UpdateServer/datareceive.php
+++ UpdateServer/datareceive.php
@@ -1,11 +1,12 @@
 <?php
 
-// http://YOUR-DOMAIN.com/datareceive.php?mac=000000000000&devicename=ExampleDevice&fwver=v1.0
+// http://dl.timfischbach.com/datareceive.php?mac=000000000000&devicename=TestProduct&fwver=v1.0&status=OK
 
  $var1 = $_GET['mac'];
  $var2 = $_GET['devicename'];
  $var3 = $_GET['fwver'];
- $fileContent = "[". date("Y-m-d H:i:s") ."] MAC: ".$var1.", Device Name: ".$var2.", Firmware Version: " .$var3. "\n";
+ $var4 = $_GET['status'];
+ $fileContent = "[". date("Y-m-d H:i:s") ."] MAC: ".$var1.", Device Name: ".$var2.", Firmware Version: " .$var3. ", Status: " .$var4. "\n";
  $fileStatus = file_put_contents('datalog.txt',$fileContent,FILE_APPEND);
  if($fileStatus != false) 
  {
