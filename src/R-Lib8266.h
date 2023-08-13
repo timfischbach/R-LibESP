4ca3ff783035f9ca7582938d8460f43eefb67ce9
Tim Fischbach <59933715+timfischbach@users.noreply.github.com>
2023-08-13 17:25:53 +0200

    Major chnages. v2

--- src/R-Lib8266.h
+++ src/R-Lib8266.h
@@ -1,5 +1,5 @@
-#ifndef RLib8266
-#define RLib8266
+#ifndef R_Lib8266_h
+#define R_Lib8266_h
 
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
@@ -8,39 +8,56 @@
 #include <ESP8266WebServer.h>
 #include <EEPROM.h>
 #include <ESP8266mDNS.h>
-#include "R-Lib8266.h"
 
-extern const String LIBVERSION;
+class R_Lib8266
+{
+public:
+    R_Lib8266();
 
-extern int progress;
 
-String checkUpdate();
-String split(String s, char parser, int index);
-String performUpdate();
-String dataTransmission();
-void connectWIFI(String ssid, String passwd);
-bool checkWIFI();
-void resetWIFI();
-void saveWIFI(String ssid, String password);
-String loadWIFI(String mode);
-void connectWIFIUser(String ssid, String password);
-void createWebServer();
-String getLibVersion();
-void connectWIFIUserHandle();
-void endWIFIUser();
-String getLibVersion();
-void connectWIFIUserHandle();
-void endWIFIUser();
-void setDeviceName(String DevName);
-String getDeviceName();
-void setVersion(String ver);
-String getVersion();
-void setDlLink(String DLL);
-String getDlLink();
-void setBetaState(bool sbeta);
-bool getBetaState();
-void setDevState(bool sdev);
-bool getDevState();
-bool varCheck();
+    String checkUpdate();
+    String secureCheckUpdate();
+    String split(String s, char parser, int index);
+    String performUpdate();
+    String securePerformUpdate();
+    String dataTransmission();
+    void connectWIFI();
+    bool checkWIFI();
+    void resetWIFI();
+    void saveWIFI(String ssid, String password);
+    void connectWIFIUser(String ssid, String password);
+    void createWebServer();
+    String getLibVersion();
+    void connectWIFIUserHandle();
+    void endWIFIUser();
+    void setDeviceName(String DevName);
+    String getDeviceName();
+    void setVersion(String ver);
+    String getVersion();
+    void setDlLink(String DLL);
+    String getDlLink();
+    void setBetaState(bool sbeta);
+    bool getBetaState();
+    void setDevState(bool sdev);
+    bool getDevState();
+    bool varCheck();
+    void saveOV(String oldversion);
+    String loadOV();
+    void update_started();
+    void update_finished();
+    void update_progress(int cur, int total);
+    void update_error(int err);
+
+private:
+
+
+    String libVersion;
+    int progress;
+    String deviceName;
+    String version;
+    String dlLink;
+    bool betaState;
+    bool devState;
+};
 
 #endif
